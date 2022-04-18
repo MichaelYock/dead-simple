8aef15ce3cb2c91f9e7248fdcad25e175ab68d0a
Michael Y <michael.yockney@gmail.com>
2022-04-18 19:00:39 +1200

    favicon png

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -8,7 +8,7 @@ const config: GatsbyConfig = {
   plugins: ["gatsby-plugin-netlify-cms", "gatsby-plugin-postcss", "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", `gatsby-plugin-styled-components`, {
     resolve: 'gatsby-plugin-manifest',
     options: {
-      "icon": "src/images/favicon.ico"
+      "icon": "src/images/favicon-32x32.png"
     }
   }, "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
     resolve: 'gatsby-source-filesystem',
